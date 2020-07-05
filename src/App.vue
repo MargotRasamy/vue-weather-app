b12270a27ae361a9529256399bed1f490bebb8c8
Margot Rasamy <rasamymargot@gmail.com>
2020-07-05 15:53:03 +0200

    Responsive page with most UI elements

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,9 @@ body{
 h1,h2,h3,h4,h5,h6 {
     font-weight: bold;
 }
+a{
+  text-decoration: none;
+}
 
 #app {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
---
+++ src/components/molecules/Nav.vue
@@ -0,0 +1,83 @@
+<template>
+
+    <nav>
+        <span></span>
+        <ul class="nav">
+            <li class="nav-item nav-active">
+            <router-link class="nav-link " to="/">Météo du jour</router-link>   
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/nextdays">Les prochains jours</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/favorites">Mes villes favorites</router-link>
+            </li>
+        </ul>
+     </nav>   
+    
+
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/scss-variables/medias.scss";
+nav {
+    background-color: var(--colorPurpleLight);
+    color: var(--colorWhite);
+    height:100%;
+    width:100%;
+    border-radius: 0 var(--borderRadiusLarge) 0 0;
+    text-align: left;
+    @include small {
+         height: max-content;
+         box-shadow: var(--boxShadow);
+        border-radius: var(--borderRadiusLarge);
+    }
+}
+span {
+    background-color: var(--colorPurpleLight);
+    position: absolute;
+    left:0;
+    bottom:0;
+    width:31px;
+    height:calc(100% - 89px);
+    @include small{
+        display: none;
+    }
+}
+.nav{
+    list-style: none;
+    width:100%;
+    padding:0;
+   margin: var(--spaceL) 0;
+}
+.nav-link{
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding:var(--spaceXS) var(--spaceM);
+    min-height:40px;
+    
+}
+.nav-active{
+    color: var(--colorPurple);
+    background-color: var(--colorPurpleVeryLight);
+
+    border-radius: var(--borderRadiusMedium) 0 0 var(--borderRadiusMedium) ;
+    @include small{
+        background-color: var(--colorPurpleVeryLight);
+        border-radius: 0;
+       
+    }
+}
+
+  
+    
+
+
+</style>
