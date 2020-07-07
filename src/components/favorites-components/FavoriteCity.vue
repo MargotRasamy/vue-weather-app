2295c6ec210f32d519ce6eb3e9af480775e574e9
Margot Rasamy <rasamymargot@gmail.com>
2020-07-07 23:39:43 +0200

    Favorite cities

---
+++ src/components/favorites-components/FavoriteCity.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="favorite-city">
+        <span>
+            <p class="favorite-city--name">London, GB</p>
+            <img class="favorite" src="../../assets/favorite.svg" height="20px"/>
+        </span>
+        <span>
+            <img class="flag" src="https://www.countryflags.io/gb/shiny/64.png"/>
+        </span>
+        
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FavoriteCity'
+}
+</script>
+
+<style scoped lang="scss">
+.favorite-city{
+    display: flex;
+    flex-direction: column;
+    padding:var(--spaceM);
+    background-color: var(--colorWhite);
+    border-radius: var(--borderRadiusMedium);
+    box-shadow: var(--boxShadowPurple);
+    & span {
+        display: float;
+        & .favorite-city--name{
+            float:left;
+            font-size: 21px;
+        }
+        & .favorite {
+            float: right;
+        }
+    }
+}
+</style>
--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="favorites">
-    fav
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
+    <FavoriteCity/>
   </div>
 </template>
 
 <script>
 
+import FavoriteCity from '../components/favorites-components/FavoriteCity.vue';
+
 export default {
-  name: 'Favorites'
+  name: 'Favorites',
+  components : {
+    FavoriteCity
+  }
 }
+
 </script>
 
 <style scoped lang="scss">
+  @import "./../assets/scss-variables/medias.scss";
 
+.favorites{
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-template-rows: repeat(4,1fr);
+  grid-gap: var(--gutterWidth);
+  @include small{
+    grid-template-columns: repeat(2,1fr);
+  }
+}
 
 </style>
